16225dc92fbc286b2acd1e062fdaac29ee191fc3
ABEWebstudio <info@abe-webstudio.de>
2025-07-07 11:48:03 +0200

    first commit

---
+++ src/static-data/navbar.ts
@@ -0,0 +1,52 @@
+import { NavbarItem } from "@/types/navbar";
+
+export const navbarData: NavbarItem[] = [
+  {
+    id: crypto.randomUUID(),
+    title: "Features",
+    href: "#features",
+    external: false
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "About",
+    href: "#about",
+    external: false
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Portfolio",
+    href: "#portfolio",
+    external: false
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Pricing",
+    href: "#pricing",
+    external: false
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Pages",
+    submenu: [
+      {
+        id: crypto.randomUUID(),
+        title: "Home",
+        href: "/",
+        external: false
+      },
+      {
+        id: crypto.randomUUID(),
+        title: "Docs",
+        href: "/docs",
+        external: false
+      },
+      {
+        id: crypto.randomUUID(),
+        title: "Support",
+        href: "/support",
+        external: false
+      },
+    ]
+  }
+];
