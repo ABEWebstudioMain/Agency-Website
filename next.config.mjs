46bd47ea5fb056ad8b8461d5f5d9f5c2020a078d
belkalaaoui94 <belkalaaoui94@gmail.com>
2025-07-07 16:41:54 +0200

    Fix App Router Internationalization

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'de'],
-    defaultLocale: 'en',
-    localeDetection: true,
-  },
   images: {
     remotePatterns: [
       {
